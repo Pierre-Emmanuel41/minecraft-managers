c2d0eb6c08433de7098268bc9d7a49493e194f48
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-29 23:20:18 +0200

    Upgrading MessageManager

--- src/main/java/fr/pederobien/minecraftmanagers/MessageManager.java
+++ src/main/java/fr/pederobien/minecraftmanagers/MessageManager.java
@@ -1,5 +1,6 @@
 package fr.pederobien.minecraftmanagers;
 
+import java.util.StringJoiner;
 import java.util.stream.Stream;
 
 import org.bukkit.Bukkit;
@@ -65,92 +66,38 @@ public class MessageManager {
 	}
 
 	/**
-	 * Send a message to the given player and with message characteristics. The value of parameter colour is not the char associated
-	 * to the {@link ChatColor} but the "human reading" name such as : red, purple, green, dark_blue...
+	 * Send all messages to the given player. This messages are concatenated together and the result is send to the player. This
+	 * method is equivalent to minecraft title commands.
 	 * 
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param player  The player that will receive the message.
-	 * @param message The message to send to the player.
-	 * @param bold    If true the message should be in bold.
-	 * @param italic  If true the message should be in italic.
-	 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
+	 * @param option   Option to send the message as title, subtitle or in actionbar.
+	 * @param player   The player that will receive the message.
+	 * @param messages An array that contains all messages to send to the player.
 	 */
-	public static void sendMessage(DisplayOption option, Player player, String message, boolean bold, boolean italic, String color) {
-		StringBuilder builder = new StringBuilder("title " + player.getName() + " ");
-		builder.append(option.toString() + " ");
-		builder.append("{\"text\":\"" + message);
-		builder.append(bold == false ? "" : "\", \"bold\":\"" + bold);
-		builder.append(italic == false ? "" : "\", \"italic\":\"" + italic);
-		builder.append(color == null ? "" : "\", \"color\":\"" + color + "\"");
-		builder.append("}");
-		BukkitManager.dispatchCommand(builder.toString());
+	public static void sendMessage(DisplayOption option, Player player, TitleMessage... messages) {
+		StringJoiner cmd = new StringJoiner(" ");
+		cmd.add("title").add(player.getName()).add(option.toString());
+		if (messages.length > 1) {
+			StringJoiner msgJoiner = new StringJoiner(", ", "[", "]");
+			msgJoiner.add("\"\"");
+			for (TitleMessage message : messages)
+				msgJoiner.add(message.toJson());
+			cmd.add(msgJoiner.toString());
+		} else {
+			cmd.add(messages[0].toJson());
+		}
+		BukkitManager.dispatchCommand(cmd.toString());
 	}
 
 	/**
-	 * Send a message to each player present in the given stream and with message characteristics using the method
-	 * {@link #sendMessage(DisplayOption, Player, String, boolean, boolean, String)}
+	 * Send all messages to the given player. This messages are concatenated together and the result is send to the player. This
+	 * method is equivalent to minecraft title commands.
 	 * 
-	 * @param players A stream that contains all players to send the specified message.
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param message The message to send to the player.
-	 * @param bold    If true the message should be in bold.
-	 * @param italic  If true the message should be in italic.
-	 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
+	 * @param option   Option to send the message as title, subtitle or in actionbar.
+	 * @param player   The player that will receive the message.
+	 * @param messages An array that contains all messages to send to the player.
 	 */
-	public static void sendMessage(Stream<Player> players, DisplayOption option, String message, boolean bold, boolean italic, String color) {
-		players.peek(player -> sendMessage(option, player, message, bold, italic, color));
-	}
-
-	/**
-	 * Send the specified message to the given player using
-	 * {@link #sendMessage(DisplayOption, Player, String, boolean, boolean, String)} with bold equals false, italic equals false and
-	 * color equals null.
-	 * 
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param player  The player that will receive the message.
-	 * @param message The message to send to the player.
-	 */
-	public static void sendMessage(DisplayOption option, Player player, String message) {
-		sendMessage(option, player, message, false, false, null);
-	}
-
-	/**
-	 * Send the specified message to the given player using
-	 * {@link #sendMessage(DisplayOption, Player, String, boolean, boolean, String)} with bold equals false, italic equals false.
-	 * 
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param player  The player that will receive the message.
-	 * @param message The message that will be displayed on the player's screen.
-	 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
-	 */
-	public static void sendMessage(DisplayOption option, Player player, String message, String color) {
-		sendMessage(option, player, message, false, false, color);
-	}
-
-	/**
-	 * Send the specified message to each player present in the given stream using
-	 * {@link #sendMessage(Stream, DisplayOption, String, boolean, boolean, String)} with bold equals false, italic equals false and
-	 * color equals null.
-	 * 
-	 * @param players A stream that contains all players to send the specified message.
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param message The message that will be displayed on the player's screen.
-	 */
-	public static void sendMessage(Stream<Player> players, DisplayOption option, String message) {
-		sendMessage(players, option, message, false, false, null);
-	}
-
-	/**
-	 * Send the specified message to the each player present in the stream using
-	 * {@link #sendMessage(Stream, DisplayOption, String, boolean, boolean, String)} with bold equals false, italic equals false.
-	 * 
-	 * @param players A stream that contains all players to send the specified message.
-	 * @param option  Option to send the message as title, subtitle or in actionbar.
-	 * @param message The message that will be displayed on the player's screen.
-	 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
-	 */
-	public static void sendMessage(Stream<Player> players, DisplayOption option, String message, String color) {
-		sendMessage(players, option, message, false, false, color);
+	public static void sendMessage(DisplayOption option, Stream<Player> players, TitleMessage... messages) {
+		players.peek(player -> sendMessage(option, player, messages));
 	}
 
 	public enum DisplayOption {
@@ -165,6 +112,114 @@ public class MessageManager {
 		@Override
 		public String toString() {
 			return name;
+		}
+	}
+
+	public static class TitleMessage {
+		private String message, color;
+		private boolean isBold, isItalic;
+
+		private TitleMessage(String message, boolean isBold, boolean isItalic, String color) {
+			this.message = message;
+			this.isBold = isBold;
+			this.isItalic = isItalic;
+			this.color = color;
+		}
+
+		/**
+		 * Create message to send to a player and with message characteristics. The value of parameter colour is not the char associated
+		 * to the {@link ChatColor} but the "human reading" name such as : red, purple, green, dark_blue...
+		 * 
+		 * @param message The message to send to the player.
+		 * @param bold    If true the message should be in bold.
+		 * @param italic  If true the message should be in italic.
+		 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
+		 * 
+		 * @return The created message based on the given parameters.
+		 */
+		public static TitleMessage of(String message, boolean isBold, boolean isItalic, String color) {
+			return new TitleMessage(message, isBold, isItalic, color);
+		}
+
+		/**
+		 * Create message to send to a player and with message characteristics. The value of parameter colour is not the char associated
+		 * to the {@link ChatColor} but the "human reading" name such as : red, purple, green, dark_blue... This message is not coloured
+		 * (white).
+		 * 
+		 * @param message The message to send to the player.
+		 * @param bold    If true the message should be in bold.
+		 * @param italic  If true the message should be in italic.
+		 * 
+		 * @return The created message based on the given parameters.
+		 */
+		public static TitleMessage of(String message, boolean isBold, boolean isItalic) {
+			return of(message, isBold, isItalic, null);
+		}
+
+		/**
+		 * Create message to send to a player and with message characteristics. The value of parameter colour is not the char associated
+		 * to the {@link ChatColor} but the "human reading" name such as : red, purple, green, dark_blue... This message is neither in
+		 * italic nor in bold.
+		 * 
+		 * @param message The message to send to the player.
+		 * @param color   If the colour is not null and the colour name is correct, then the message is displayed with the given colour.
+		 * 
+		 * @return The created message based on the given parameters.
+		 */
+		public static TitleMessage of(String message, String color) {
+			return of(message, false, false, color);
+		}
+
+		@Override
+		public String toString() {
+			StringJoiner joiner = new StringJoiner(", ", "{", "}");
+			joiner.add("text:" + message);
+			joiner.add("bold:" + isBold);
+			joiner.add("italic:" + isItalic);
+			joiner.add("color:" + color);
+			return joiner.toString();
+		}
+
+		public String toJson() {
+			StringJoiner joiner = new StringJoiner(", ", "{", "}");
+			join(joiner, true, "text", message);
+			join(joiner, isBold, "bold", true);
+			join(joiner, isItalic, "italic", true);
+			join(joiner, color != null, "color", color);
+			return joiner.toString();
+		}
+
+		/**
+		 * @return The message of to send to send to a player.
+		 */
+		public String getMessage() {
+			return message;
+		}
+
+		/**
+		 * @return The color of this message.
+		 */
+		public String getColor() {
+			return color;
+		}
+
+		/**
+		 * @return True if the message is in <b>bold</b>, false otherwise.
+		 */
+		public boolean isBold() {
+			return isBold;
+		}
+
+		/**
+		 * @return True if this message is in <i>italic</i>, false otherwise.
+		 */
+		public boolean isItalic() {
+			return isItalic;
+		}
+
+		private void join(StringJoiner joiner, boolean condition, String key, Object value) {
+			if (condition)
+				joiner.add("\"" + key + "\":\"" + value + "\"");
 		}
 	}
 }
